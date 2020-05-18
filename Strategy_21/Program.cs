4d68e073b3b16f02fecda4a17a76f62a8830eb25
aleksej <aleksej.shherbak@yandex.ru>
2020-05-19 00:51:11 +0300

    Strategy

---
+++ Strategy_21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Strategy_21
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sort = new SelectionSort();
+            var context = new Context(sort);
+            context.Sort();
+            context.PrintArray();
+        }
+    }
+}
